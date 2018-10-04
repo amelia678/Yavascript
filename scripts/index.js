3b25e2ce631d5c0692199f17d91661cafefe7f46
Amelia Schulz email = amelia.schulz@gmail.com <ameliaschulz@Amelias-MacBook-Pro.local>
2018-10-04 15:07:23 -0400

    prompts, lets, consts, and interpolates

---
+++ scripts/index.js
@@ -0,0 +1,29 @@
+// Yep, another comment style
+
+// console.log("hello javascript what up");
+
+// hello.py
+// getting input(it's lame)
+
+// using var
+// // and camelCase
+// var promptText = 'What is your name?';
+// var userName = prompt(promptText);
+
+// // uses string concatenation
+// var greeting = 'Hello, ' + userName + '!';
+// console.log(greeting);
+
+// now using sexy syntaxxx
+// using let and const
+// rule of thumb: use const until you get an error
+
+const promptText = 'What is your name?'
+const userName = prompt(promptText);
+// string interpolation
+// 1. replace quotes with backticks
+// 2. remove the + signs
+// 3. wrap your variables in ${}
+const greeting = `Hello, ${userName} !`;
+console.log(greeting);
+
